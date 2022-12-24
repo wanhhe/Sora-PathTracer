641f50dbdc441fc48831865937e898f6d61b5102
sora <wanhhe@qq.com>
2022-12-24 17:50:34 +0800

    内置bunny场景

--- src/scene.cpp
+++ src/scene.cpp
@@ -34,7 +34,32 @@ void Scene::loadSceneBox(Config* config) {
 }
 
 void Scene::loadSceneBunny(Config* config) {
+	auto redDiffuseMaterial = make_shared<DiffuseMaterial>(vec3(0.63, 0.065, 0.05));
+	auto greenDiffuseMaterial = make_shared<DiffuseMaterial>(vec3(0.14, 0.45, 0.091));
+	auto whiteDiffuseMaterial = make_shared<DiffuseMaterial>(vec3(0.725, 0.71, 0.68));
+	auto lightMaterial = make_shared<DiffuseLightMaterial>(vec3(23.918, 19.283, 15.541));
 
+	shared_ptr<Material> bunnyMaterial;
+	switch (config->material)
+	{
+	case DIFFUSE: bunnyMaterial = whiteDiffuseMaterial; break;
+	case MIRROR: bunnyMaterial = make_shared<MirrorMaterial>(vec3(0.725, 0.71, 0.68)); break;
+	case MICROFACET: bunnyMaterial = make_shared<MicrofacetMaterial>(vec3(0.725, 0.71, 0.68), config->roughness);
+	default:
+		throw std::runtime_error("Material not supported");
+	}
+
+	auto floor = new Object("../models/cornellbox/floor.obj", whiteDiffuseMaterial);
+	auto left = new Object("../models/cornellbox/left.obj", redDiffuseMaterial);
+	auto right = new Object("../models/cornellbox/right.obj", greenDiffuseMaterial);
+	auto light = new Object("../models/cornellbox/light.obj", lightMaterial);
+	auto bunny = new Object("../models/bunny/bunny.obj", bunnyMaterial);
+
+	objects.emplace_back(floor);
+	objects.emplace_back(left);
+	objects.emplace_back(right);
+	objects.emplace_back(light);
+	objects.emplace_back(bunny);
 }
 
 void Scene::buildNaive() {
@@ -46,5 +71,5 @@ void Scene::buildBVH() {
 }
 
 void Scene::buildSAH() {
-
+	accelStructure = new SAHAccelStructure(objects);
 }
