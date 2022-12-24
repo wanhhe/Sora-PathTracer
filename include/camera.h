129e1551d129ae2eeed3b31a6cab2815268f86a0
sora <wanhhe@qq.com>
2022-12-24 23:18:05 +0800

    添加随机发射光线函数

--- include/camera.h
+++ include/camera.h
@@ -2,14 +2,18 @@
 #define PATHTRACER_CAMERA_H
 
 #include "common.h"
+#include "ray.h"
 
 class Camera {
 public:
 	Camera(vec3 lookFrom, vec3 lookTarget, vec3 up, float fov, float aspectRation);
+	Ray getRayRandom(int row, int col);
+
 private:
 	vec3 origin;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 upperLeftCorner;
 };
 
 #endif // !PATHTRACER_CAMERA_H
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,13 +1,26 @@
 #include "camera.h"
 
 Camera::Camera(vec3 lookFrom, vec3 lookTarget, vec3 up, float fov, float aspectRation) {
-	auto theta = radians(fov);
-	auto viewHeight = 2.0;
+	float theta = radians(fov);
+	auto h = tan(theta / 2.0);
+	auto viewHeight = 2.0 * h;
 	auto viewWidth = viewHeight * aspectRation;
 
 	// ÉãÏñ»úµÄ×ø±ê u,v,w
 	auto w = normalize(lookFrom - lookTarget);
 	auto u = normalize(cross(up, w));
 	auto v = cross(w, u);
+
 	origin = lookFrom;
+	horizontal = (float)viewWidth * u;
+	vertical = (float)viewHeight * v;
+	upperLeftCorner = origin - w - horizontal / 2.f - vertical / 2.f;
+}
+
+Ray Camera::getRayRandom(int row, int col) {
+	auto sample = linearRand(vec2(0.0), vec2(1.0));
+	float rowScale = (float)row + sample.x;
+	float colScale = (float)col + sample.y;
+	auto lookAt = upperLeftCorner + vertical * rowScale / (float)SCREEN_HEIGHT + horizontal * colScale / (float)SCREEN_WIDTH;
+	return Ray(origin, lookAt - origin);
 }
